9cf6c6a59c6d3f62c95254602a58383b4fcf3cf9
bisw4sh <spikeystona@gmail.com>
2025-05-10 14:03:45 +0545

    feat: styled and exported layout from the mdx page itself

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,7 @@ import AboutMDX from "./About.mdx"
 export default function About() {
   return (
     <main className="min-h-screen lg:px-[19rem] pt-16">
-      <section className="prose prose-neutral">
-        <AboutMDX />
-      </section>
+      <AboutMDX />
     </main>
   )
 } 
