bdfd28b2f1eefe42ca092182a2eaa95ab4249f87
bisw4sh <spikeystona@gmail.com>
2025-06-17 18:55:36 +0545

    fix: removed the node-postgres driver for neon-database

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,15 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+//
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+//
+// export const db = drizzle({ client: pool });
+//
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-export const db = drizzle({ client: pool });
-
-
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
