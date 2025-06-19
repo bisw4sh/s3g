f167b29abc2c9c0d5a223b8e77fd56d35f8b638b
bisw4sh <spikeystona@gmail.com>
2025-06-19 19:47:38 +0545

    chores: session expiry and updating

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,5 +22,9 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
+  }
 });
 
