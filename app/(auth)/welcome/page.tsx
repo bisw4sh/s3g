ed3c2eaec8b4a0e4777807079d4661cd64930f28
bisw4sh <spikeystona@gmail.com>
2025-06-19 18:21:14 +0545

    feat: welcome and errors paeg

---
+++ app/(auth)/welcome/page.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <main className="min-h-screen lg:px-[19rem] pt-16">
+      Welcome
+    </main>
+  )
+}
---
+++ app/errors/page.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <main className="min-h-screen lg:px-[19rem] pt-16">
+      Error Page
+    </main>
+  )
+}
