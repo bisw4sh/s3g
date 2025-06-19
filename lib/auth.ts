01e1cebd62f8c6090f509d6b7b669a1b61617cfb
bisw4sh <spikeystona@gmail.com>
2025-06-19 21:39:36 +0545

    chores: pkg and betterAuth plugin for otp

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { schema } from "@/db/schema";
+import { emailOTP } from "better-auth/plugins"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -25,6 +26,16 @@ export const auth = betterAuth({
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
-  }
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        console.log({ email, otp, type })
+        // Implement the sendVerificationOTP method to send the OTP to the user's email address
+      },
+      otpLength: 4,
+      expiresIn: 1000
+    })
+  ]
 });
 
