97489a3b8105f34b915cbd45fceec606856dcd4f
bisw4sh <spikeystona@gmail.com>
2025-06-21 20:11:13 +0545

    feat: forgetPassword sequence and redirection

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Suspense } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import {
@@ -14,14 +14,15 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 const forgotEmailSchema = z.object({
-  email: z.string().email()
+  email: z.string().min(3, { message: "Email is required" }).email()
 })
 type TForgotPassword = z.infer<typeof forgotEmailSchema>
 
 const ForgotPasswordComponent = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
   const form = useForm<TForgotPassword>({
     resolver: zodResolver(forgotEmailSchema),
@@ -32,8 +33,14 @@ const ForgotPasswordComponent = () => {
 
   async function onSubmit(values: TForgotPassword) {
     try {
-      console.log(values)
-      router.push(`/message?type=info&heading=Check+your+email&content=Password+reset+link+has+been+sent+to+your+email+address+${values.email}`);
+      const { data, error } = await authClient.forgetPassword({
+        email: values.email,
+        redirectTo: `http://localhost:3000/reset-password`,
+      });
+      console.log("values", values)
+      console.log("data", data)
+      console.log("error", error)
+      // router.push(`/message?type=info&heading=Check+your+email&content=Password+reset+link+has+been+sent+to+your+email+address+${values.email}`);
     } catch (error) {
       console.error("Password reset failed:", error);
     }
@@ -42,7 +49,7 @@ const ForgotPasswordComponent = () => {
   return (
     <main className="min-h-screen flex justify-center items-center inset-shadow-white">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/4 h-1/2">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-6 md:w-1/2 lg:w-1/4 h-1/2">
           <h1 className="text-xl font-semibold">Forgot Password/ Reset Password Change Page</h1>
           <FormField
             control={form.control}
