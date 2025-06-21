fe25deecdd6310a223205bd10dd809a7f73aaeec
bisw4sh <spikeystona@gmail.com>
2025-06-21 13:34:31 +0545

    feat: forgot password page

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { Suspense } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const forgotEmailSchema = z.object({
+  email: z.string().email()
+})
+type TForgotPassword = z.infer<typeof forgotEmailSchema>
+
+const ForgotPasswordComponent = () => {
+  const router = useRouter();
+
+  const form = useForm<TForgotPassword>({
+    resolver: zodResolver(forgotEmailSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+
+  async function onSubmit(values: TForgotPassword) {
+    try {
+      console.log(values)
+      router.push(`/message?type=info&heading=Check+your+email&content=Password+reset+link+has+been+sent+to+your+email+address+${values.email}`);
+    } catch (error) {
+      console.error("Password reset failed:", error);
+    }
+  };
+
+  return (
+    <main className="min-h-screen flex justify-center items-center inset-shadow-white">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/4 h-1/2">
+          <h1 className="text-xl font-semibold">Forgot Password/ Reset Password Change Page</h1>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Email to reset password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="cursor-pointer">Send Email</Button>
+        </form>
+      </Form>
+    </main>
+  );
+};
+
+export default function ForgotPasswordPage() {
+  return (
+    <Suspense>
+      < ForgotPasswordComponent />
+    </Suspense>
+  );
+}
+
